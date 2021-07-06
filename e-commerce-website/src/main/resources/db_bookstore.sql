7db8df309c786987984cc7cc2b100f6ed8dc4635
Tu Kien Vinh <vinh.tukien@gmail.com>
2021-07-06 22:47:57 +0700

    Add new tables to database

--- e-commerce-website/src/main/resources/db_bookstore.sql
+++ e-commerce-website/src/main/resources/db_bookstore.sql
@@ -8,7 +8,7 @@ create table author (
 	id int not null default nextval('author_sequence') primary key,
 	name varchar(250) not null,
 	country varchar(250),
-	rating decimal(10, 1)
+	rating decimal(10, 1) not null
 );
 	
 drop table if exists publisher cascade;
@@ -47,9 +47,10 @@ create table product (
 	author_id int references author (id),
 	description text not null,
 	price numeric check(price > 0) not null,
+	stock int check(stock > 0) not null,
 	image varchar(250),
-	created_date date not null,
-	updated_date date
+	created_date timestamp not null,
+	updated_date timestamp
 );
 
 drop table if exists roles cascade;
@@ -73,4 +74,35 @@ create table customer (
 	id int not null default nextval('customer_sequence') primary key,
 	name varchar(250) not null,
 	role_id int references roles (id)
+);
+
+drop table if exists shopping_cart;
+
+create table shopping_cart (
+	customer_id int not null references customer (id) primary key,
+	product_id int not null references product (id),
+	product_name varchar(250) not null,
+	stock int check (stock > 0) not null
+);
+
+drop table if exists orders cascade;
+
+drop sequence if exists orders_sequence;
+
+create sequence orders_sequence increment 1 start 1;
+
+create table orders (
+	id int not null default nextval('orders_sequence') primary key,
+	customer_id int references customer (id) not null,
+	order_time timestamp not null
+);
+
+drop table if exists order_detail;
+
+create table order_detail (
+	id int references orders (id) not null primary key,
+	customer_id int references customer (id) not null,
+	product_id int not null references product (id),
+	product_name varchar(250) not null,
+	stock int not null
 );
