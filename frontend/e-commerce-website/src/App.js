30fa2b67bef758d1e17600d07a6e7c12aaf60def
Tu Kien Vinh <vinh.tukien@gmail.com>
2021-07-20 00:45:16 +0700

    Add navbar + 404 page

--- frontend/e-commerce-website/src/App.js
+++ frontend/e-commerce-website/src/App.js
@@ -2,8 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import NotFound from './components/404page';
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 class App extends React.Component {
   state = {
@@ -17,13 +18,16 @@ class App extends React.Component {
   render() {
     return (
       <Router>
-        <div className="App">
-        <Navbar onSearchKey={(e) => this.handleSearchKey(e)} />
-          
-          <Route exact path="/">
-            <Home bootcamp={this.state.bootcamp}/>
-          </Route>
-      </div>
+          <div className="App">
+            <Navbar onSearchKey={(e) => this.handleSearchKey(e)} />
+            <Switch>
+              <Route exact path="/">
+                <Home bootcamp={this.state.bootcamp}/>
+              </Route>
+
+              <Route path = "**" render = {() => <NotFound/>}></Route>
+            </Switch>
+          </div>
       </Router>
     );
   }
--- frontend/e-commerce-website/src/components/Navbar/index.js
+++ frontend/e-commerce-website/src/components/Navbar/index.js
@@ -1,33 +1,38 @@
 import React, { Component } from 'react'
 import { Link } from "react-router-dom";
 import './Navbar.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FaSearch } from 'react-icons/fa';
 
 export default class index extends Component {
-  state = {
-    name: "Vinh"
-  };
-
   render() {
     return (
       <div>
         <nav id='navbar'>
-        <ul>
-              <Link to="/">
-                <li>Home</li>
-              </Link>
-              <Link to="/contact">
-                <li>Contact</li>
-              </Link>
-              <Link to="/about">
-                <li>About</li>
-              </Link>
-            </ul>
+          <ul>
+            <Link to="/">
+              <li>RookieShop</li>
+            </Link>
+            <Link to="/categories">
+              <li>Categories</li>
+            </Link>
+            <Link to="/products">
+              <li>Products</li>
+            </Link>
+          </ul>
 
-          <input type="text" onChange={(e) => this.props.onSearchKey(e)} />
-
-          <div className="nav-details">
-            <p className="nav-username"> {this.state.name} </p>
+          <div class="search-container">
+            <form action="/search">
+              <input type="text" placeholder="Search for your book.." name="search"/>
+              <button type="submit"><FaSearch/></button>
+            </form>
           </div>
+
+          <ul>
+            <Link to="/signin">
+              <li>Sign in</li>
+            </Link>
+          </ul>
         </nav>
       </div>
     );
